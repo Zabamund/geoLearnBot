01dee41b0297b26cba05392e2126e4b7cbcde638
Zabamund <fracgeol@gmail.com>
2017-03-21 09:16:36 +0100

    scraping tests and some changes to bot

--- webScrapping/scrapeTest.js
+++ webScrapping/scrapeTest.js
@@ -1,20 +1,37 @@
-// code author https://github.com/IonicaBizau/scrape-it
+// code modified from https://www.codementor.io/johnnyb/how-to-write-a-web-scraper-in-nodejs-du108266t
 
-const scrapeIt = require('scrape-it');
+"use strict"
 
-// Promise interface
-scrapeIt("https://mineralseducationcoalition.org/minerals-database/andesite/", {
-    dataBaseEntry: ".mec-db-entry__title",
-    // specimenInfo: ".mec-db-entry__details-grid-cell",
-    listItem: ".acf-image-gallery__item",
-    data: {
-        href: ".acf-image-gallery__link wwm_socialshare_imagewrapper",
-    }
-}).then(page => {
-    console.log(page);
+// Import the dependencies
+const cheerio = require("cheerio"), req = require("tinyreq");
+
+const url = 'https://mineralseducationcoalition.org/minerals-database/diorite/';
+
+// Define the scrape function
+function scrape(url, data, callback) {
+    // 1. Create the request
+    req(url, (error, body) => {
+        if (error) { return callback(error); }
+
+        // 2. Parse the HTML
+        let $ = cheerio.load(body), pageData = {};
+
+        // 3. Extract the data
+        Object.keys(data).forEach(key => {
+            pageData[key] = $(data[key]).text();
+        });
+
+        // Send the data in the callback
+        callback(null, pageData);
+    });
+}
+
+// Extract some data from my website
+// Get the website title (from the top header)
+// ...and the description
+scrape(url, {
+    title: ".header h1",
+    description: ".header h2"
+}, (error, data) => {
+    console.log(error || data);
 });
-
-
-<a href="https://mineralseducationcoalition.org/wp-content/uploads/andesite_366069737.jpg" class="acf-image-gallery__link wwm_socialshare_imagewrapper" data-featherlight="image" style="width: 101px;">
-    <img src="https://mineralseducationcoalition.org/wp-content/uploads/andesite_366069737-150x150.jpg" alt="" class="acf-image-gallery__image mec-share-image"><ul class="wwm_social_share wwm_top_left" style="display: none;"><li class="wwm_facebook"></li><li class="wwm_twitter"></li><li class="wwm_pinit"></li></ul>
-</a>
--- webScrapping/scrapper_Manel/scrapper.js
+++ webScrapping/scrapper_Manel/scrapper.js
@@ -6,34 +6,37 @@ const mineralsArray = ['aluminum', 'amphibole', 'andesite', 'antimony', 'aragoni
 let DATABASE = [];
 
 function minerals(mineral){
-  scrapeIt(`https://mineralseducationcoalition.org/minerals-database/${mineral}/`, {
-      TITLE: "h1.mec-db-entry__title",
-      DESCRIPTION: {
-        selector: ".entry-content p",
-        eq: 0
-      },
-      IMG: {
-        listItem: "li.acf-image-gallery__item",
-        data: {
-            url: {
-              selector: "a",
-              attr: "href"
-            },
-            img: {
-              selector: "img",
-              attr: "src"
+    scrapeIt(`https://mineralseducationcoalition.org/minerals-database/${mineral}/`, {
+        TITLE: "h1.mec-db-entry__title",
+        DESCRIPTION: {
+            // what is entry-content?
+            selector: ".entry-content p",
+            // what is this eq: 0 ?
+            eq: 0
+        },
+        IMG: {
+            listItem: "li.acf-image-gallery__item",
+            data: {
+                url: {
+                    selector: "a",
+                    attr: "href"
+                },
+                img: {
+                    selector: "img",
+                    attr: "src"
+                }
             }
         }
-      }
-  }).then(page => {
-      DATABASE.push(page);
-  });
+    }).then(page => {
+        DATABASE.push(page);
+    });
 }
 
+// loops through array of minerals and calls scrape function
 mineralsArray.forEach((el) => minerals(el));
 
 setTimeout(function(){
-  console.log("Our database length: ", DATABASE.length);
-  console.log("Our database is: ", DATABASE);
-  console.log("The first element is: ", DATABASE[0]);
-}, 1500);
+    console.log("Our database length: ", DATABASE.length);
+    console.log("Our database is: ", DATABASE);
+    console.log("The first element is: ", DATABASE[0]);
+}, 3000);
