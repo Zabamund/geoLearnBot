a9400535eca4d530aa21db3ce597e100e1caae7b
Propulsion <Propulsion>
2017-03-22 16:48:35 +0100

    small improvements to bot and code cleaning

--- GeoBot/src/main/java/geoLearnBot/GeoLearnBot.java
+++ GeoBot/src/main/java/geoLearnBot/GeoLearnBot.java
@@ -26,14 +26,17 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 		// check if the update has a message and the message has text
 		if (update.hasMessage() && update.getMessage().hasText()) {
 
-			// /start command
-			if (update.getMessage().getText().equals("/start")) {
+			// /start || 1
+			if (update.getMessage().getText().equals("/start") || update.getMessage().getText().equals("1")) {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText(
 						// @formatter:off
-								"*Would you like to learn or play?*"
-								+ "\n\n*1*. Learn !"
-								+ "\n*2*. Play ! (sorry, I'm not implemented yet...)"
-								+ "\n*3*. Acknowledgments")
+								"*You can discover this Bot by picking a number or with the /menu to the right of your text input*"
+								+ "\n\n*1*. Show start menu"
+								+ "\n*2*. Need help ?"
+								+ "\n*3*. Learn about some minerals"
+								+ "\n*4*. Play ! (sorry, nothing here yet...)"
+								+ "\n*5*. Acknowledgments"
+								+ "\n*6*. Glossary")
 								// @formatter:on
 						.enableMarkdown(true);
 				try {
@@ -43,8 +46,8 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				}
 			}
 
-			// /help command
-			if (update.getMessage().getText().equals("/help")) {
+			// /help || 2
+			if (update.getMessage().getText().equals("/help") || update.getMessage().getText().equals("2")) {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText(
 						// @formatter:off
 								"*Here is what this bot can do:*"
@@ -59,9 +62,43 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				}
 			}
 
-			// /acknowledgments command
+			// /learn || 3
+			if (update.getMessage().getText().equals("/learn") || update.getMessage().getText().equals("3")) {
+				// String mineralContent = mineralsList.get(0).toString();
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						.setText(
+								// @formatter:off
+									mineralsList.get(0).toString())						
+									// @formatter:on
+						.enableHtml(true);
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Play || 4
+			if (update.getMessage().getText().equals("/quiz") || update.getMessage().getText().equals("4")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// .setReplyMarkup(ReplyKeyboardMarkup)
+						.setText(
+								// @formatter:off
+										"*Booo ! nothing here yet :-(*")						
+									// @formatter:on
+						.enableMarkdown(true);
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /acknowledgments || 5
 			if (update.getMessage().getText().equals("/acknowledgements")
-					|| update.getMessage().getText().equals("3")) {
+					|| update.getMessage().getText().equals("5")) {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId()).setText(
 						// @formatter:off
 								"*geoLearnBot Acknowledges and Credits*"
@@ -80,30 +117,15 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				}
 			}
 
-			// /learn || 1
-			if (update.getMessage().getText().equals("/learn") || update.getMessage().getText().equals("1")) {
-				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
-						.setText(
-						// @formatter:off
-						mineralsList.get(0).toString())						
-						// @formatter:on
-						.enableMarkdown(true);
-				try {
-					sendMessage(message);
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-
-			}
-
-			// /quiz || 2
-			if (update.getMessage().getText().equals("/quiz") || update.getMessage().getText().equals("2")) {
+			// /Glossary || 6
+			if (update.getMessage().getText().equals("/glossary") || update.getMessage().getText().equals("6")) {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// .setReplyMarkup(ReplyKeyboardMarkup)
 						.setText(
-						// @formatter:off
-							"*quiz placeholder*")						
-						// @formatter:on
+								// @formatter:off
+								"So you need to learn about some of the terms I use when showing you minerals ?"
+								+ "\nThat's totally cool, I'm all about teaching so here we go!")						
+								// @formatter:on
 						.enableMarkdown(true);
 				try {
 					sendMessage(message);
