e6dfbf50fa8a62db9f749b36b6b647de9d1af979
Zabamund <fracgeol@gmail.com>
2017-03-23 11:57:18 +0100

    custom keyboard implementation and addition of glossary

--- GeoBot/src/main/java/geoLearnBot/GeoLearnBot.java
+++ GeoBot/src/main/java/geoLearnBot/GeoLearnBot.java
@@ -52,14 +52,14 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 
 			// /help || 2
 			if (update.getMessage().getText().equals("/help") || update.getMessage().getText().equals("2")) {
-				String winky = "\\U+1F609";
+				String winky = "\ud83d\ude09";
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						.setText(
 						// @formatter:off
 								"*Here is what this bot can do:*"
 								+ "\n\nType \"/\" for main options (start, help, learn, quiz, acknowledgments or glossary)"
 								+ "\nOnce inside the main options use 1, 2, 3, 4, 5 and 6 to navigate the bot, have fun !"
-								+ "\n\nErrors: if you keep asking for more minerals and they're not coming, well...why don't you read the text and wait a little " + winky + " ?")
+								+ "\n\n*Errors:* if you keep asking for more minerals and they're not coming, well...why don't you read the text and wait a little " + winky + " ?")
 								// @formatter:on
 						.enableMarkdown(true);
 				try {
@@ -165,11 +165,32 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 
 			// /Mineral Classification
 			if (update.getMessage().getText().equals("Mineral Classification")) {
+
+				KeyboardRow keyboardRowUpper = new KeyboardRow();
+				keyboardRowUpper.add(0, "Carbonate");
+				keyboardRowUpper.add(1, "Halide");
+				keyboardRowUpper.add(2, "Native");
+				keyboardRowUpper.add(3, "Phyllosilicate");
+
+				KeyboardRow keyboardRowLower = new KeyboardRow();
+				keyboardRowLower.add(0, "Pyroxene");
+				keyboardRowLower.add(1, "Silicate");
+				keyboardRowLower.add(2, "Sulfate");
+				keyboardRowLower.add(3, "Sulfide");
+
+				List<KeyboardRow> keyboard = new ArrayList<>();
+				keyboard.add(keyboardRowUpper);
+				keyboard.add(keyboardRowLower);
+
+				ReplyKeyboardMarkup replyMarkup = new ReplyKeyboardMarkup();
+				replyMarkup.setKeyboard(keyboard).setOneTimeKeyboad(true);
+
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// @formatter:off
 						.setText(
-								"booyah MinClass !!")
-						.enableMarkdown(true);
+								"Ok, which term do you want to learn about ?")
+						.enableMarkdown(true)
+						.setReplyMarkup(replyMarkup);
 						// @formatter:on
 				try {
 					sendMessage(message);
@@ -178,12 +199,166 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				}
 			}
 
+			// /Carbonate
+			if (update.getMessage().getText().equals("Carbonate")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"Carbonate minerals are those minerals containing the carbonate ion: CO3\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Halide
+			if (update.getMessage().getText().equals("Halide")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"The halide mineral class include those minerals with a dominant halide anion (F−, Cl−, Br− and I−)."
+								+ "\nComplex halide minerals may also have polyatomic anions in addition to or that include halides."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Native
+			if (update.getMessage().getText().equals("Native")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"Native element minerals are those elements that occur in nature in uncombined form with a distinct mineral structure."
+								+ " The elemental class includes metals and intermetallic elements, naturally occurring alloys, semi-metals and non-metals."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Phyllosilicate
+			if (update.getMessage().getText().equals("Phyllosilicate")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"Phyllosilicates are sheet Silicate minerals, formed by parallel sheets of silicate tetrahedra with Si2O5 or a 2:5 ratio."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Pyroxene
+			if (update.getMessage().getText().equals("Pyroxene")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"The pyroxenes (commonly abbreviated to Px) are a group of important rock-forming inosilicate minerals"
+								+ " found in many igneous and metamorphic rocks. Pyroxenes are silicon-aluminum oxides with Ca, Na, Fe,"
+								+ " Mg, Zn, Mn, Li substituting for Si and Al."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Silicate
+			if (update.getMessage().getText().equals("Silicate")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"Silicate minerals are rock-forming minerals made up of silicate groups. They are the largest"
+								+ " and most important class of rock-forming minerals and make up approximately 90 percent of"
+								+ " the Earth's crust. They are classified based on the structure of their silicate groups,"
+								+ " which contain different ratios of silicon and oxygen."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Sulfate
+			if (update.getMessage().getText().equals("Sulfate")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"The sulfate minerals are a class of minerals which include the sulfate ion (SO42−) within"
+								+ " their structure. The sulfate minerals occur commonly in primary evaporite depositional"
+								+ " environments, as gangue minerals in hydrothermal veins and as secondary minerals in the"
+								+ " oxidizing zone of sulfide mineral deposits."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// /Sulfide
+			if (update.getMessage().getText().equals("Sulfide")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"The sulfide minerals are a class of minerals containing sulfide (S2−) as the major anion."
+								+ " Some sulfide minerals are economically important as metal ores. "
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+
+			}
+
 			// /Streak
 			if (update.getMessage().getText().equals("Streak")) {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// @formatter:off
 						.setText(
-								"booyah Streak !!")
+								"The streak (also called \"powder color\") of a mineral is the color of the powder produced"
+								+ " when it is dragged across an un-weathered surface. Unlike the apparent color of a mineral,"
+								+ " which for most minerals can vary considerably, the trail of finely ground powder generally"
+								+ " has a more consistent characteristic color, and is thus an important diagnostic tool in"
+								+ " mineral identification. If no streak seems to be made, the mineral's streak is said to be "
+								+ "white or colorless. Streak is particularly important as a diagnostic for opaque and colored "
+								+ "materials. It is less useful for silicate minerals, most of which have a white streak or are"
+								+ " too hard to powder easily."
+								+ "\n_Source: Wikipedia_")
 						.enableMarkdown(true);
 						// @formatter:on
 				try {
@@ -198,7 +373,10 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// @formatter:off
 						.setText(
-								"booyah Mohs Hardness !!")
+								"The Mohs scale of mineral hardness is a qualitative ordinal scale characterizing scratch "
+								+ "resistance of various minerals through the ability of harder material to scratch softer"
+								+ " material."
+								+ "\n_Source: Wikipedia_")
 						.enableMarkdown(true);
 						// @formatter:on
 				try {
@@ -210,10 +388,175 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 
 			// /Crystal System
 			if (update.getMessage().getText().equals("Crystal System")) {
+
+				KeyboardRow keyboardRowUpper = new KeyboardRow();
+				keyboardRowUpper.add(0, "Triclinic");
+				keyboardRowUpper.add(1, "Monoclinic");
+				keyboardRowUpper.add(2, "Orthorhombic");
+				keyboardRowUpper.add(3, "Tetragonal");
+
+				KeyboardRow keyboardRowLower = new KeyboardRow();
+				keyboardRowLower.add(0, "Hexagonal");
+				keyboardRowLower.add(1, "Trigonal");
+				keyboardRowLower.add(2, "Isometric");
+
+				List<KeyboardRow> keyboard = new ArrayList<>();
+				keyboard.add(keyboardRowUpper);
+				keyboard.add(keyboardRowLower);
+
+				ReplyKeyboardMarkup replyMarkup = new ReplyKeyboardMarkup();
+				replyMarkup.setKeyboard(keyboard).setOneTimeKeyboad(true);
+
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// @formatter:off
 						.setText(
-								"booyah Crystals !!")
+								"In crystallography, the terms crystal system, crystal family and lattice system"
+								+ " each refer to one of several classes of space groups, lattices, point groups"
+								+ " or crystals."
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true)
+						.setReplyMarkup(replyMarkup);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Triclinic
+			if (update.getMessage().getText().equals("Triclinic")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"In crystallography, the triclinic (or anorthic) crystal system is one of the 7 "
+								+ "crystal systems. A crystal system is described by three basis vectors. In the "
+								+ "triclinic system, the crystal is described by vectors of unequal length, as in "
+								+ "the orthorhombic system. In addition, no vector is at right angles (90°) "
+								+ "orthogonal to another."
+								+ "\nhttps://goo.gl/Ka29tv"
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Monoclinic
+			if (update.getMessage().getText().equals("Monoclinic")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"In crystallography, the monoclinic crystal system is one of the 7 crystal systems."
+								+ " A crystal system is described by three vectors. In the monoclinic system, the "
+								+ "crystal is described by vectors of unequal lengths, as in the orthorhombic system."
+								+ " They form a rectangular prism with a parallelogram as its base. Hence two vectors"
+								+ " are perpendicular (meet at right angles), while the third vector meets the other"
+								+ " two at an angle other than 90°."
+								+ "\nhttps://goo.gl/Yosn2m"
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Orthorhombic
+			if (update.getMessage().getText().equals("Orthorhombic")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"In crystallography, the orthorhombic crystal system is one of the 7 crystal systems. "
+								+ "Orthorhombic lattices result from stretching a cubic lattice along two of its orthogonal "
+								+ "pairs by two different factors, resulting in a rectangular prism with a rectangular"
+								+ " base (a by b) and height (c), such that a, b, and c are distinct. All three bases"
+								+ " intersect at 90° angles, so the three lattice vectors remain mutually orthogonal."
+								+ "\nhttps://goo.gl/t6lSl1"
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Tetragonal
+			if (update.getMessage().getText().equals("Tetragonal")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"In crystallography, the tetragonal crystal system is one of the 7 crystal systems."
+								+ " Tetragonal crystal lattices result from stretching a cubic lattice along one of "
+								+ "its lattice vectors, so that the cube becomes a rectangular prism with a square"
+								+ " base (a by a) and height (c, which is different from a)."
+								+ "\nhttps://goo.gl/8EvD4K"
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Hexagonal
+			if (update.getMessage().getText().equals("Hexagonal")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"In crystallography, the hexagonal crystal family is one of the 6 crystal families. "
+								+ "In the hexagonal family, the crystal is conventionally described by a right rhombic"
+								+ " prism unit cell with two equal axes (a by a), an included angle of 120° (gamma) and a "
+								+ "height (c, which can be different from a) perpendicular to the two base axes."
+								+ "\nhttps://goo.gl/txDjSc"
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Trigonal
+			if (update.getMessage().getText().equals("Trigonal")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"The trigonal crystal system is the only crystal system whose point groups have"
+								+ " more than one lattice system associated with their space groups: the hexagonal"
+								+ " and rhombohedral lattices both appear."
+								+ "\nhttps://goo.gl/txDjSc"
+								+ "\n_Source: Wikipedia_")
+						.enableMarkdown(true);
+						// @formatter:on
+				try {
+					sendMessage(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// /Isometric(cubic)
+			if (update.getMessage().getText().equals("Isometric")) {
+				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
+						// @formatter:off
+						.setText(
+								"In crystallography, the cubic (or isometric) crystal system is a crystal system where"
+								+ " the unit cell is in the shape of a cube. This is one of the most common and simplest "
+								+ "shapes found in crystals and minerals."
+								+ "\nhttps://goo.gl/OnpkIM"
+								+ "\n_Source: Wikipedia_")
 						.enableMarkdown(true);
 						// @formatter:on
 				try {
@@ -228,7 +571,10 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// @formatter:off
 						.setText(
-								"booyah Luster !!")
+								"Lustre or luster is the way light interacts with the surface of a crystal,"
+								+ " rock, or mineral. The word traces its origins back to the latin lux,"
+								+ " meaning \"light\", and generally implies radiance, gloss, or brilliance."
+								+ "A range of terms are used to describe lustre, such as earthy, metallic, greasy, and silky.")
 						.enableMarkdown(true);
 						// @formatter:on
 				try {
@@ -243,7 +589,10 @@ public class GeoLearnBot extends TelegramLongPollingBot {
 				SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId())
 						// @formatter:off
 						.setText(
-								"booyah Fracture !!")
+								"In the field of mineralogy, fracture is the texture and shape of a rock's "
+								+ "surface formed when a mineral is fractured. Minerals often have a highly"
+								+ " distinctive fracture, making it a principal feature used in their"
+								+ " identification.")
 						.enableMarkdown(true);
 						// @formatter:on
 				try {
