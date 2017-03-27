58e2ebd259444c05f82d23dc79f6b497aba9f411
Zabamund <fracgeol@gmail.com>
2017-03-27 07:58:06 +0200

    closing testing branch for now

---
+++ GeoBot/src/test/java/geoBotTests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class geoBotTests {
+
+	// define update to be able to test functionality
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
